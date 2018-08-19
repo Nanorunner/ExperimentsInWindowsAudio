835a9252e07646e90ea9175c47f603cfe6f3a1a4
Enders Kong <konge@live.com>
2018-08-19 14:45:25 -0700

    Added alternate method of finding indicies.

--- showAudioDeviceIndicies.py
+++ showAudioDeviceIndicies.py
@@ -1,5 +1,6 @@
 import pyaudio
 import os
+import sounddevice as sd
 
 chunk = 1024
 FORMAT = pyaudio.paInt16
@@ -17,3 +18,11 @@ def grabDeviceIndex(search):
     for x in range(100):
         if p.get_device_info_by_index(x)["name"].find(search) != -1:
             return p.get_device_info_by_index(x)["index"]
+def grabDeviceIndex2(search):
+    deviceList = list(sd.query_devices())
+    counter = 0
+    for x in deviceList:
+        if search in x["name"]:
+            return counter
+        counter += 1
+    print("No such device with %s in its name." % (search))
